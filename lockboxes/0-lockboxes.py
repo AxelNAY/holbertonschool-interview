ede9bca3ddea5b4c9d96ad7105f3369addd8fca4
AxelNAY <axelnay@sfr.fr>
2025-04-04 15:03:59 +0200

    add comparaison for the size of the boxe

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Write a method that determines if all the boxes can be opened."""
 
+
 def canUnlockAll(boxes):
     """Prototype: def canUnlockAll(boxes)
     boxes is a list of lists
@@ -13,15 +14,17 @@ def canUnlockAll(boxes):
     compare = [0]
     for i in range(len(boxes) - 1):
         if len(boxes[i]) == 0:
-                return False
-        for j in range (len(boxes[i])):
+            return False
+        for j in range(len(boxes[i])):
             if boxes[i][j] <= len(boxes):
-                for k in range (len(compare)):
+                for k in range(len(compare)):
                     if boxes[i][j] == compare[k]:
                         verify = 1
                 if verify == 0:
                     compare.append(boxes[i][j])
                 verify = 0
+                if len(compare) == len(boxes):
+                    return True
     if len(compare) != len(boxes):
         return False
     return True
