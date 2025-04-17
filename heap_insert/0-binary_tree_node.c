30315cda8b4d3435f876117dfc99d79d220165e9
AxelNAY <axelnay@sfr.fr>
2025-04-17 10:41:39 +0200

    Fix betty style for file 0-binary_tree_node.c

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -8,8 +8,8 @@
 * @parent: Pointer to the parent node of the node to create
 * @value: Value to put in the new node
 */
-
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
     binary_tree_t *new = malloc(sizeof(binary_tree_t));
 
 	if (new == NULL)
