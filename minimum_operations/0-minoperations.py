c983e2f452579eebf8381020a72a2d92b6918768
AxelNAY <axelnay@sfr.fr>
2025-04-25 15:13:41 +0200

    Complete 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,26 +1,54 @@
 #!/usr/bin/python3
+"""Calculates the fewest number of operations needed
+to result in exactly n H characters"""
 
 
 def minOperations(n):
     letter = "H"
     characters = [letter]
     copy = 1
-    operation = -1
-    count = 0
+    operation = 0
+    i = 0
 
     if n <= 0:
         return (0)
+    if n == 1:
+        return (1)
 
-    while (len(characters) < n):
-        i = 0
-        if count % 2 == 0:
-            copy = len(characters)
-            operation = operation + 2
-        else:
-            operation = operation + 1
+    if n % 5 == 0:
+        operation = operation + 5
+        copy = 4
         while (i < copy):
             characters.append(letter)
             i = i + 1
-        count = count + 1
+    elif n % 3 == 0:
+        operation = operation + 3
+        copy = 2
+        while (i < copy):
+            characters.append(letter)
+            i = i + 1
+    elif n % 2 == 0:
+        operation = operation + 2
+        copy = 1
+        while (i < copy):
+            characters.append(letter)
+            i = i + 1
+
+    if operation != 0:
+        while len(characters) != n:
+            i = 0
+            if n % len(characters) == 0:
+                operation = operation + 2
+                copy = len(characters)
+            else:
+                operation = operation + 1
+            while (i < copy):
+                characters.append(letter)
+                i = i + 1
+    else:
+        operation = 1
+        while len(characters) != n:
+            operation = operation + 1
+            characters.append(letter)
 
     return (operation)
