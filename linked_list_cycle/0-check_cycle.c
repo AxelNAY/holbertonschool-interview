bd33dc2e9b9f84510eeb0000fa71d95991831846
AxelNAY <axelnay@sfr.fr>
2025-05-23 13:14:47 +0200

    Add and complete all files in linked_list_cycle folder

---
+++ linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+
+/**
+* check_cycle - check if list is a cycle
+* @list: list to check
+*
+* Return: 1 if Success of 0 if Fail
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *temp, *node_storage;
+	int i = 0, j;
+
+	temp = list;
+
+	node_storage = malloc(sizeof(list));
+	if (node_storage == NULL)
+	{
+		return (0);
+	}
+
+	while (temp != NULL)
+	{
+		node_storage = list;
+		j = 0;
+		while (j < i)
+		{
+			if (temp->n == node_storage->n)
+			{
+				return (1);
+			}
+			node_storage = node_storage->next;
+			j++;
+		}
+		temp = temp->next;
+		i++;
+	}
+
+	return (0);
+}
