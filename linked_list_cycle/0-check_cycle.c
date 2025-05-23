59594c9527a7c17b3324c46a7380b84f157ed969
AxelNAY <axelnay@sfr.fr>
2025-05-23 14:32:24 +0200

    Betty style

--- linked_list_cycle/0-check_cycle.c
+++ linked_list_cycle/0-check_cycle.c
@@ -9,25 +9,29 @@
 *
 * Return: 1 if Success of 0 if Fail
 */
-int check_cycle(listint_t *list) {
-    listint_t *temp, *node_storage;
-    int i = 0, j;
+int check_cycle(listint_t *list)
+{
+	listint_t *temp, *node_storage;
+	int i = 0, j;
 
-    temp = list;
+	temp = list;
 
-    while (temp != NULL) {
-        node_storage = list;
-        j = 0;
-        while (j < i) {
-            if (temp->n == node_storage->n) {
-                return 1;
-            }
-            node_storage = node_storage->next;
-            j++;
-        }
-        temp = temp->next;
-        i++;
-    }
+	while (temp != NULL)
+	{
+		node_storage = list;
+		j = 0;
+		while (j < i)
+		{
+			if (temp->n == node_storage->n)
+			{
+				return (1);
+			}
+			node_storage = node_storage->next;
+			j++;
+		}
+		temp = temp->next;
+		i++;
+	}
 
-    return 0;
+	return (0);
 }
