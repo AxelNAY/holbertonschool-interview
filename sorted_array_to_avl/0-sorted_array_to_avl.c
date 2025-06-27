99e1f70446eb3cdeb25b6645e7e775b3104636c7
AxelNAY <axelnay@sfr.fr>
2025-06-27 14:20:29 +0200

    Complete all files in sorted_array_to_avl folder

--- sorted_array_to_avl/0-sorted_array_to_avl.c
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -13,5 +13,46 @@
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
+	if (array == NULL || size == 0)
+		return (NULL);
+	return (merge_algo(array, 0, size - 1, NULL));
+}
 
+/**
+ * merge_algo - a function that use recursion to sort the nodes of an
+ * AVL tree from a sorted array
+ *
+ * @array: pointer to array containing elements
+ *
+ * @left_start: start of the left index
+ *
+ * @right_end: end of the right index
+ *
+ * @root: pointer to the parent node which is NULL
+ *
+ * Return: newly created node and it's values
+ */
+avl_t *merge_algo(int *array, int left_start, int right_end, avl_t *root)
+{
+	int middle;
+	avl_t *tree;
+
+	if (left_start > right_end)
+		return (NULL);
+
+	/* step 1 */
+	middle = (left_start + right_end) / 2;
+
+	/* step 2 and 3*/
+	tree = calloc(1, sizeof(avl_t));
+	if (tree == NULL) /* step 4 */
+		return (NULL);
+
+	tree->parent = root; /* step 5 */
+	/* step 6.. */
+	tree->left = merge_algo(array, left_start, middle - 1, tree);
+	tree->right = merge_algo(array, middle + 1, right_end, tree);
+	tree->n = array[middle]; /* step 7 */
+
+	return (tree);
 }
--- sorted_array_to_avl/binary_trees.h
+++ sorted_array_to_avl/binary_trees.h
@@ -17,4 +17,6 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
 
+void binary_tree_print(const binary_tree_t *tree);
 avl_t *sorted_array_to_avl(int *array, size_t size);
+avl_t *merge_algo(int *array, int left_start, int right_end, avl_t *root);
