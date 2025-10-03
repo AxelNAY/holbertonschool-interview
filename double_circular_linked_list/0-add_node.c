944114e7be59a0ecc3e136bc6949652c2db1a2fa
AxelNAY <axelnay@sfr.fr>
2025-10-03 14:29:33 +0200

    Creation double_circular_linked_list folder and his files

---
+++ double_circular_linked_list/0-add_node.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+/**
+ * add_node_end - Add a new node to the end of a double circular linked list
+ *
+ * @list: list to modify
+ * @str: string to copy into the new node
+ *
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+	List *newNode, *last;
+
+	if (!str || !list)
+		return (NULL);
+
+	newNode = malloc(sizeof(List));
+	if (!newNode)
+		return (NULL);
+
+	newNode->str = str;
+	newNode->next = newNode->prev = NULL;
+
+	if (!*list)
+	{
+		newNode->next = newNode->prev = newNode;
+		*list = newNode;
+	}
+	else
+	{
+		last = (*list)->prev;
+
+		newNode->next = *list;
+		newNode->prev = last;
+		last->next = newNode;
+		(*list)->prev = newNode;
+	}
+
+	return (newNode);
+}
+
+
+/**
+ * add_node_begin - Add a new node to the beginning
+ * of a double circular linked list
+ * @list: list to modify
+ * @str: string to copy into the new node
+ *
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_begin(List **list, char *str)
+{
+	List *newNode, *last;
+
+	if (!str || !list)
+		return (NULL);
+
+	newNode = malloc(sizeof(List));
+	if (!newNode)
+		return (NULL);
+
+	newNode->str = str;
+	newNode->next = newNode->prev = NULL;
+
+	if (!*list)
+	{
+		newNode->next = newNode->prev = newNode;
+		*list = newNode;
+	}
+	else
+	{
+		last = (*list)->prev;
+
+		newNode->next = *list;
+		newNode->prev = last;
+		(*list)->prev = newNode;
+		last->next = newNode;
+		*list = newNode;
+	}
+
+	return (newNode);
+}
