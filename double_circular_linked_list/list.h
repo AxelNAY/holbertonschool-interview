5b902993db1c5b41c1358a2e3be86616502ef0bd
AxelNAY <axelnay@sfr.fr>
2025-10-03 15:14:59 +0200

    Update for empty list 0-add_node.c

--- double_circular_linked_list/list.h
+++ double_circular_linked_list/list.h
@@ -1,3 +1,6 @@
+#ifndef LIST_H
+#define LIST_H
+
 /**
  * struct List - doubly linked list
  * @str: string - (malloc'ed string)
@@ -9,10 +12,12 @@
  */
 typedef struct List
 {
-  char *str;
-  struct List *prev;
-  struct List *next;
+	char *str;
+	struct List *prev;
+	struct List *next;
 } List;
 
 List *add_node_end(List **list, char *str);
 List *add_node_begin(List **list, char *str);
+
+#endif /* LIST_H */
