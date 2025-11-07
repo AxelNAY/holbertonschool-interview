1ada51ca44e4b6194a4138e531a746602e44542c
AxelNAY <axelnay@sfr.fr>
2025-11-07 11:05:23 +0100

    Creation of the pascal_triangle folder and his files

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 """Contain the function pascal_triangle."""
 
+from math import factorial
+
+
 def pascal_triangle(n):
-    """Returns a list of lists of integers representing the Pascal’s triangle of n"""
+    """Returns a list of lists of integers
+    representing the Pascal’s triangle of n"""
     triangle = []
     if (n <= 0):
         return triangle
 
-    
+    for i in range(n):
+        row = []
+        for j in range(i+1):
+            row.append(factorial(i)//(factorial(j)*factorial(i-j)))
+        triangle.append(row)
 
+    return triangle
